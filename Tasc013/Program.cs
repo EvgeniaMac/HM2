773abc6a4757a8d87f37e807763937a827ed01d0
«Evgenia <evgenia.kopitext@gmail.com>
2022-06-30 21:17:06 +1100

    tasc013

---
+++ Tasc013/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if(result=(N%100 - N%10)/10)
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Нет второго числа");
+}
